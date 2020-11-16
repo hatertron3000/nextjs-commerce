c5896a3dea4ad65bddb18b273ef4a3c2ccaeec7b
hatertron3000 <patrick.puente@c02wv0avhtdh.lan>
2020-11-16 17:17:55 -0600

    Google Analytics

--- components/common/Head/Head.tsx
+++ components/common/Head/Head.tsx
@@ -10,6 +10,20 @@ const Head: FC = () => {
       <NextHead>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
+
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Q89V7HRRGG"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-Q89V7HRRGG', {
+  'linker': {
+    'domains': ['nextjs-test-store.mybigcommerce.com']
+  }
+});`
+        }} />
       </NextHead>
     </>
   )
